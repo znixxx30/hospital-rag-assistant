f3303d424679f4dbde1dd618be38fb75bcff37a8
znixxx30 <anshuman261001@gmail.com>
2026-04-03 03:03:51 +0530

    Implemented free local embeddings using sentence-transformers

--- app/ingest.py
+++ app/ingest.py
@@ -1,28 +1,35 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
 
-def load_and_split_pdf(file_path: str):
+def load_split_embed(file_path: str):
+    # Load PDF
     loader = PyPDFLoader(file_path)
     documents = loader.load()
 
-    print(f"Loaded {len(documents)} pages")
-
+    # Chunking
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
         chunk_overlap=100
     )
-
     chunks = splitter.split_documents(documents)
 
-    print(f"Total chunks created: {len(chunks)}")
+    print(f"Total chunks: {len(chunks)}")
 
-    # Preview first few chunks
-    for i, chunk in enumerate(chunks[:5]):
-        print(f"\n--- Chunk {i+1} ---")
-        print(chunk.page_content)
+    # Load free embedding model
+    model = SentenceTransformer("all-MiniLM-L6-v2")
 
-    return chunks
+    # Convert chunks → embeddings
+    texts = [chunk.page_content for chunk in chunks]
+    vectors = model.encode(texts)
+
+    # Preview
+    for i in range(2):
+        print(f"\n--- Sample Embedding {i+1} ---")
+        print(vectors[i][:10])
+
+    return chunks, vectors
 
 
 if __name__ == "__main__":
-    chunks = load_and_split_pdf("data/ai_ml_doc_assignement.pdf")
+    chunks, vectors = load_split_embed("data/ai_ml_doc_assignement.pdf")
